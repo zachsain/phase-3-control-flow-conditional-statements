37229bada77b9c5ba064a01408af9b0ecf209605
Zachary Sain <zachsain@gmail.com>
2022-03-18 14:49:54 -0500

    commit tree b24160a917e46d3ea8c6cb1f439d70c46af31971

--- control_flow.rb
+++ control_flow.rb
@@ -1,14 +1,32 @@
+require "pry"
+
 def admin_login(username, password)
-  # your code here
+  if (username == "admin" || username == "ADMIN") && password == "12345"
+    "Access granted"
+  else 
+    "Access denied"
+  end
 end
 
 def hows_the_weather(temperature)
   # your code here
+  
 end
 
 def fizzbuzz(num)
-  # your code here
+  if num % 3 == 0 && num % 5 == 0
+   
+    "FizzBuzz"
+  elsif num % 5 == 0
+    "Buzz"
+  elsif num % 3 == 0 
+   "Fizz"
+  else
+    num 
+  end
 end
+
+# fizzbuzz(15)
 
 def calculator(operation, num1, num2)
   # your code here
